d65df2a6a808421866c7827332a8d766d1e71c9e
Michael Dwyer <Mike.a.dwyer32@gmail.com>
2021-05-02 13:28:31 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+// Write your solution in this file
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+    if (bestCustomer === 'not bob') {
+        return bestCustomer
+    }
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+    if (bestCustomer === 'maybe bob') {
+        return bestCustomer
+    }
+}
+
+const leastFavoriteCustomer = 'billy bob'
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'jeff'
+    if (leastFavoriteCustomer === 'jeff') {
+        return leastFavoriteCustomer
+    }
+}
